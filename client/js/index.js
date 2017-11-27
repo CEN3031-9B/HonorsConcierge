6f3a895204d3359ef5947f042c9fe19b9bda7b92
Caio Garcia Hernandez <caiohernandez24@gmail.com>
2017-11-27 13:14:24 -0500

    Updated HTML files to be included with Angular

---
+++ client/js/index.js
@@ -0,0 +1,19 @@
+// function deleteSearch (x) {
+//   document.getElementsByName('search')[0].placeholder = "";
+//   //window.getComputedStyle(document.querySelector('.search'), '::after').getPropertyValue('opacity');
+// }
+//
+// function addSearch (x) {
+//   document.getElementsByName('search')[0].placeholder = "Search...";
+//   //document.getElementsByClassName('search')[0].style.opacity = 0.8;
+// }
+
+// $(function() {
+//     $('.card').hover(
+//         function() {
+//             $(this).find('> .card-content > span.card-title').click();
+//         }, function() {
+//             $(this).find('> .card-reveal > .card-title').click();
+//         }
+//     );
+// });
