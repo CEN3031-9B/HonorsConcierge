f1d702e83198a4d68735680f7b0fd9b05378f593
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-11-28 04:18:19 -0500

    added children and ancestor functionality

--- client/app/tree/tree.component.js
+++ client/app/tree/tree.component.js
@@ -3,6 +3,8 @@ const angular = require('angular');
 
 const uiRouter = require('angular-ui-router');
 
+const async = require('async');
+
 import routes from './tree.routes';
 
 export class TreeComponent {
@@ -19,26 +21,43 @@ export class TreeComponent {
   $onInit() {
     this.treeService.getTreeInfo(this.$stateParams.treeId)
       .then(res => {
+        this.$scope.currId = res.data._id;
         this.$scope.currTitle = res.data.title;
         this.$scope.currDesc = res.data.description;
         this.$scope.currContent = res.data.content;
-        this.$scope.currChildren = res.data.children;
+        this.$scope.currChildrenIds = res.data.children;
         this.$scope.currAncestors = res.data.ancestors;
+        this.$scope.currChildren = [];
+        async.each(this.$scope.currChildrenIds, (childId, cb) => {
+          this.treeService.getTreeInfo(childId)
+            .then(innerRes => {
+              this.$scope.currChildren.push(innerRes.data);
+              cb();
+            });
+        });
       }, () => {
         this.$window.location.href = '/';
       });
   }
 
   addNode() {
-    this.treeService.addNode({
+    const newAncestors = angular.copy(this.$scope.currAncestors);
+    newAncestors.push({
+      title: this.$scope.nodeAncestorTitle,
+      id: this.$scope.currId
+    });
+    const newNode = {
       title: this.$scope.nodeTitle,
       description: this.$scope.nodeDesc,
       content: this.$scope.nodeContent,
       children: [],
-      ancestors: this.$scope.nodeAncestor
-    }).then(res => {
-      this.$scope.currChildren.push(res.data._id);
-      this.treeService.updateNodeChildren(this.$stateParams.treeId, this.$scope.currChildren)
+      ancestors: newAncestors
+    };
+    this.treeService.addNode(newNode).then(res => {
+      this.$scope.currChildrenIds.push(res.data._id);
+      newNode._id = res.data._id;
+      this.$scope.currChildren.push(newNode);
+      this.treeService.updateNodeChildren(this.$stateParams.treeId, this.$scope.currChildrenIds)
         .then(() => {
           this.$scope.message = 'Node added successfully';
         }, () => {
--- server/api/node/node.model.js
+++ server/api/node/node.model.js
@@ -3,12 +3,17 @@
 import mongoose from 'mongoose';
 import {registerEvents} from './node.events';
 
+var AncestorSchema = new mongoose.Schema({
+  title: String, // text displayed in breadcrumbs
+  id: mongoose.Schema.ObjectId // id of breadcrumx item
+})
+
 var NodeSchema = new mongoose.Schema({
   title: String, // text to be displayed for parent node
   description: String, // text to be displayed in headings and breadcrumbs
   content: String, // text to be displayed when on this node
   children: [mongoose.Schema.ObjectId],
-  ancestors: [Object] // objects should contain the name of the ancestor and the ancestor's id
+  ancestors: [AncestorSchema] // objects should contain the name of the ancestor and the ancestor's id
 });
 
 registerEvents(NodeSchema);
